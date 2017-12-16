83344ea5d4f72a3b96bdc5b3cbb7b2b4eb0ddada
Mohamadali Ahi <Mohamadali.Ahi@lut.fi>
2017-12-16 18:58:30 +0200

    DataWrngling2-2.2

--- create_human2.R
+++ create_human2.R
@@ -9,3 +9,21 @@ hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_22
 
 gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
 
+str(hd)
+dim(hd)
+summary(hd)
+
+str(gii)
+dim(gii)
+summary(gii)
+
+colnames(hd)
+
+colnames(hd) <- c("HDI.rank", "Country", "HDI", "Life.Exp", "Edu.Exp", "Edu.Mean", "GNI", "GNI.rank")
+
+colnames(gii)
+
+colnames(gii) <- c("GII.Rank", "Country", "GII", "Mat.Mor", "Ado.Birth", "Parli.F", "Edu2.F", "Edu2.M", "Labo.F", "Labo.M")
+
+
+
