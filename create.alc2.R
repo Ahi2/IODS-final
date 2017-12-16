bab308d8a922471b70ced7b94314271a70f1f6ad
Mohamadali Ahi <Mohamadali.Ahi@lut.fi>
2017-12-16 12:55:47 +0200

    wrangling-update2

--- create.alc2.R
+++ create.alc2.R
@@ -1,5 +1,5 @@
 Name: Mohamadali Ahi
 Email: mohamadali.ahi@helsinki.fi
-Data: UCI Machine Learning Repository, Student Performance Data: https://archive.ics.uci.edu/ml/datasets/Student+Performance
+Data: UCI Machine Learning Repository, Student Performance Data: [linked here](https://archive.ics.uci.edu/ml/datasets/Student+Performance)
 This is the R-Script file to do data wrangling to prepare the data for further analysis. 
 
