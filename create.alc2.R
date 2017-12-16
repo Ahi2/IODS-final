1c5e9dfd036be01cd1dd37b7d1517cd41f99a710
Mohamadali Ahi <Mohamadali.Ahi@lut.fi>
2017-12-16 12:38:16 +0200

    Uodate-Dec16th-12:36

---
+++ create.alc2.R
@@ -0,0 +1,5 @@
+Name: Mohamadali Ahi
+Email: mohamadali.ahi@helsinki.fi
+Data: UCI Machine Learning Repository, Student Performance Data: 
+This is the R-Script file to do data wrangling to prepare the data for further analysis. 
+
